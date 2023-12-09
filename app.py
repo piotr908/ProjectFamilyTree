48088bf71f6aaacee01524d162ad2f6c35815134
piotr908 <97662743+piotr908@users.noreply.github.com>
2023-12-09 20:31:46 +0100

    commit

--- app.py
+++ app.py
@@ -3,60 +3,78 @@ from database import execute_read_query, execute_write_query
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
-    nodes = execute_read_query("MATCH (n) RETURN n.id, n.name, n.MOTHER, n.FATHER, n.PARTNER")
-    return render_template('home.html', nodes = nodes)
+    query = "MATCH (n) RETURN n.id, n.name, n.MOTHER, n.FATHER, n.PARTNER"
+    nodes = execute_read_query(query)
+    return render_template('home.html', nodes=nodes)
 
 @app.route('/addperson', methods=['GET', 'POST'])
-def addPerson():
-    females = execute_read_query("MATCH (a:Person WHERE a.sex = 'Female') return a.id, a.name")
-    males = execute_read_query("MATCH (a:Person WHERE a.sex = 'Male') return a.id, a.name")
-    nodes = execute_read_query("MATCH (n) return n.id, n.name")
+def add_person():
+    query_female = "MATCH (a:Person WHERE a.sex = 'Female') return a.id, a.name"
+    females = execute_read_query(query_female)
+    query_male = "MATCH (a:Person WHERE a.sex = 'Male') return a.id, a.name"
+    males = execute_read_query(query_male)
+    query_nodes = "MATCH (n) return n.id, n.name"
+    nodes = execute_read_query(query_nodes)
+
     if request.method == 'POST':
-        person_name = request.form.get("name")
-        person_sex = request.form.get("sex")
-        person_father = request.form.get("father")
-        person_mother = request.form.get("mother")
-        person_partner = request.form.get("partner")
+        new_person = {
+            'name': request.form.get("name"),
+            'sex': request.form.get("sex"),
+            'father': request.form.get("father"),
+            'mother': request.form.get("mother"),
+            'partner': request.form.get("partner")
+        }
 
-        if(person_name != ""):
-            query = "CREATE (p1:Person { name: '%s', id: apoc.create.uuid(), sex: '%s'}) RETURN p1.id" % (person_name, person_sex)
-            id = execute_write_query(query)
-            if(person_mother != ""):
-                mother = execute_read_query("MATCH (mother:Person) WHERE mother.id = '%s' RETURN mother.name" %(person_mother))
-                query = "MATCH (mother:Person) WHERE mother.id = '%s' MATCH (person:Person) WHERE person.id = '%s' CREATE (mother)-[:MOTHER]->(person)-[:CHILD]->(mother) SET person.MOTHER = '%s'" %(person_mother, id[0][0], mother[0][0])
-                execute_write_query(query)
-                        
-            if(person_father != ""):
-                father = execute_read_query("MATCH (father:Person) WHERE father.id = '%s' RETURN father.name" %(person_father))
-                query = "MATCH (father:Person) WHERE father.id = '%s' MATCH (person:Person) WHERE person.id = '%s' CREATE (father)-[:FATHER]->(person)-[:CHILD]->(father) SET person.FATHER = '%s'" %(person_father, id[0][0], father[0][0])
-                execute_write_query(query)
+        if new_person['name']:
+            create_query = "CREATE (p1:Person {name: '%s', id: apoc.create.uuid(), sex: '%s'}) RETURN p1.id" % (new_person['name'], new_person['sex'])
+            person_id = execute_write_query(create_query)[0][0]
 
-            if(person_partner != ""):
-                partner = execute_read_query("MATCH (partner:Person) WHERE partner.id = '%s' RETURN partner.name" %(person_partner))
-                query = "MATCH (partner:Person) WHERE partner.id = '%s' MATCH (person:Person) WHERE person.id = '%s' CREATE (partner)-[:PARTNER]->(person)-[:PARTNER]->(partner) SET person.PARTNER = '%s' SET partner.PARTNER = '%s'" %(person_partner, id[0][0], partner[0][0], person_name)
-                execute_write_query(query)
+            for role in ['mother', 'father']:
+                if new_person[role]:
+                    parent_name = execute_read_query(f"MATCH (parent:Person) WHERE parent.id = '{new_person[role]}' RETURN parent.name")[0][0]
+                    relation_query = f"""
+                    MATCH (parent:Person) WHERE parent.id = '{new_person[role]}'
+                    MATCH (person:Person) WHERE person.id = '{person_id}'
+                    CREATE (parent)-[:{role.upper()}]->(person)-[:CHILD]->(parent)
+                    SET person.{role.upper()} = '{parent_name}'
+                    """
+                    execute_write_query(relation_query)
+
+                if new_person['partner']:
+                    partner_name = execute_read_query(f"MATCH (partner:Person) WHERE partner.id = '{new_person['partner']}' RETURN partner.name")[0][0]
+                    partner_query = f"""
+                    MATCH (partner:Person) WHERE partner.id = '{new_person['partner']}'
+                    MATCH (person:Person) WHERE person.id = '{person_id}'
+                    CREATE (partner)-[:PARTNER]->(person)-[:PARTNER]->(partner)
+                    SET person.PARTNER = '{partner_name}', partner.PARTNER = '{new_person['name']}'
+                    """
+                    execute_write_query(partner_query)
 
         return redirect(url_for('home'))
-    return render_template('addPerson.html', females = females, males = males, nodes = nodes)
 
-@app.route('/findconnection', methods=['GET', 'POST'])
-def findConnection():
+    return render_template('addPerson.html', females=females, males=males, nodes=nodes)
+
+@app.route('/findrelation', methods=['GET', 'POST'])
+def find_relation():
     nodes = execute_read_query("MATCH (n) return n.id, n.name")
     if request.method == 'POST':
-        person_1_id = request.form.get("person1")
-        person_2_id = request.form.get("person2")
-        resu =[]
-        if(person_1_id != "" and person_2_id != ""):
-            query = "MATCH (startNode:Person{id:'%s'}), (endNode:Person{id:'%s'}), p=shortestPath((startNode)-[*..15]-(endNode)) RETURN [x in nodes(p) | x.id]" %(person_1_id, person_2_id)
-            outcome_ids = execute_read_query(query)
-            for id in outcome_ids[0][0]:
-                resu.append(execute_read_query("MATCH (person:Person) WHERE person.id = '%s' RETURN person.id, person.name, person.MOTHER, person.FATHER, person.PARTNER" %id))
-        return render_template('findconnection.html',nodes = nodes, results = resu)
-    return render_template('findconnection.html', nodes = nodes)
+        person_1_id, person_2_id = request.form.get("person1"), request.form.get("person2")
+        results = []
 
+        if person_1_id and person_2_id:
+            relation_query = f"""
+            MATCH (startNode:Person{{id:'{person_1_id}'}}), (endNode:Person{{id:'{person_2_id}'}}), p=shortestPath((startNode)-[*..15]-(endNode))
+            RETURN [x in nodes(p) | x.id]
+            """
+            outcome_ids = execute_read_query(relation_query)[0][0]
+            for id in outcome_ids:
+                person_query = f"MATCH (person:Person) WHERE person.id = '{id}' RETURN person.id, person.name, person.MOTHER, person.FATHER, person.PARTNER"
+                results.append(execute_read_query(person_query))
 
+        return render_template('findrelation.html', nodes=nodes, results=results)
+    return render_template('findrelation.html', nodes=nodes)
+
+if __name__ == "__main__":
+    app.run(debug=True)
