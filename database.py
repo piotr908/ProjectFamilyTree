b546cb6995ff7bd62143a9c0790c1cce82ec809b
piotr908 <97662743+piotr908@users.noreply.github.com>
2023-12-09 19:35:46 +0100

    commit

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ class Database:
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
 
     def close(self):
-        self.driver.close()         # Don't forget to close the driver connection when you are finished with it
+        self.driver.close()
 
     def read_query(self, query):
         def cypher(tx, cypher):
@@ -29,9 +29,9 @@ class Database:
         with self.driver.session(database="neo4j") as session:
             return session.write_transaction(cypher, query)
 
-uri = "neo4j+s://068d36af.databases.neo4j.io"
+uri = "neo4j+s://061a40a0.databases.neo4j.io"
 user = "neo4j"
-password = "vZC1Ql9DS_83iGnumKpl4KJzximfhJ3yBiTO3F16dZk"
+password = "4JDojWLGA-f5KHju2S8KZpgt3-n2l9DnoPi_TaqLuWw"
 app = Database(uri, user, password)
 
 def execute_read_query(query):
